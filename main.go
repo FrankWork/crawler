afc4ff871e03b86c06b526439adad7a02697f35c
FrankWork <lzh00776@163.com>
2017-05-28 21:35:25 +0800

    url

--- main.go
+++ main.go
@@ -9,16 +9,24 @@ import (
 func main() {
 	fmt.Println("Hello World")
 
-	rawurl := "http://www.163.com"
+	rawurl := "http://www.163.com:80/index?page=1#active"
+	// scheme://[userinfo@]host[:port]/path[?query][#fragment]
+	// scheme:opaque[?query][#fragment]
 
 	u, err := url.Parse(rawurl)
 	if err != nil {
 		log.Fatal(err)
 	}
-	u.Scheme = "https"
-	u.Host = "google.com"
-	q := u.Query()
-	q.Set("q", "golang")
-	u.RawQuery = q.Encode()
-	fmt.Println(u)
+	fmt.Println(u.Scheme)
+	fmt.Println(u.Host)
+	fmt.Println(u.Path)
+	fmt.Println(u.Fragment)
+	fmt.Println(u.RawQuery)
+
+	// u.Scheme = "https"
+	// u.Host = "google.com"
+	// q := u.Query()
+	// q.Set("q", "golang")
+	// u.RawQuery = q.Encode()
+	// fmt.Println(u)
 }
